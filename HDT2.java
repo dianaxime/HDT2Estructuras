b94c2c27b417456eb1c505492542e54cf13cf6fc
dianaxime <35496688+dianaxime@users.noreply.github.com>
2019-01-31 05:25:44 -0600

    Add files via upload

--- HDT2.java
+++ HDT2.java
@@ -9,13 +9,38 @@ package hdt2;
  *
  * @author DIANA
  */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 public class HDT2 {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String archivo; 
+        
+        FileReader lector;
+        BufferedReader lector1;
+        String datos;
+        try{
+            lector = new FileReader(archivo);
+            lector1 = new BufferedReader(lector);
+            while ((datos=lector1.readLine())!=null){
+            
+            }  
+        }
+        catch(FileNotFoundException e){
+            
+        }
+        catch(IOException e){
+            
+        }
+        
+        
+        
     }
     
 }
